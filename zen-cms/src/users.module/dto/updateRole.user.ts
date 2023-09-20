fe2adc08d4257163b07f56790d23046d3742fbd2
wei107 <weimulun@126.com>
2023-09-20 12:44:11 +0800

    2023.9.20

--- zen-cms/src/users.module/dto/updateRole.user.ts
+++ zen-cms/src/users.module/dto/updateRole.user.ts
@@ -1,4 +1,4 @@
-import { IsArray, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { IsArray, IsNotEmpty, IsOptional, IsString, NotEquals } from "class-validator";
 
 export class UpdateRoleUserDto{
     //这里不校验写权限是否具有读权限作为前置，客户端会有相应的输入限制，并且就算是在客户端以外的地方发起请求，也要先过鉴权这一关，并且这种输入也不会对系统造成影响。
@@ -9,6 +9,7 @@ export class UpdateRoleUserDto{
     @IsString()
     @IsNotEmpty()
     @IsOptional()
+    @NotEquals("super")
     rolename: string
 
     @IsArray()
